3a71a7e2d228c32fa3dcf2bc1012f32d58d06f8e
adrianblfe <adrianblfe@gmail.com>
2022-05-26 15:57:35 +0200

    Fin sección 11

--- 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
+++ 04-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -3,15 +3,21 @@ import { NgModule } from '@angular/core';
 // PrimeNg
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
-import { MenubarModule } from 'primeng/menubar';
 import { FieldsetModule } from 'primeng/fieldset';
+import { MenubarModule } from 'primeng/menubar';
+import { TableModule } from 'primeng/table';
+import { ToolbarModule } from 'primeng/toolbar';
+
+
 
 @NgModule({
   exports: [
     ButtonModule,
-    CardModule,
     FieldsetModule,
-    MenubarModule
+    CardModule,
+    MenubarModule,
+    TableModule,
+    ToolbarModule
   ]
 })
 export class PrimeNgModule { }
--- 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
+++ 04-pipesApp/src/app/ventas/pages/ordenar/ordenar.component.ts
@@ -1,16 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+
+import { Color, Heroe } from '../../interfaces/ventas.interfaces';
 
 @Component({
-  selector: 'app-ordenar',
-  templateUrl: './ordenar.component.html',
-  styles: [
-  ]
+	selector: 'app-ordenar',
+	templateUrl: './ordenar.component.html',
+	styles: [
+	]
 })
-export class OrdenarComponent implements OnInit {
+export class OrdenarComponent {
 
-  constructor() { }
+	enMayusculas: boolean = false;
 
-  ngOnInit(): void {
-  }
+	ordenarPor: string = '';
+	heroes: Heroe[] = [
+		{
+			nombre: 'Superman',
+			vuela: true,
+			color: Color.azul,
+		},
+		{
+			nombre: 'Batman',
+			vuela: false,
+			color: Color.negro,
+		},
+		{
+			nombre: 'Robin',
+			vuela: false,
+			color: Color.verde,
+		},
+		{
+			nombre: 'Daredevil',
+			vuela: true,
+			color: Color.rojo,
+		},
+		{
+			nombre: 'Linterna verde',
+			vuela: true,
+			color: Color.verde,
+		}
+	]
 
+	toggleMayusculas() {
+		this.enMayusculas = !this.enMayusculas;
+	}
+
+	cambiarOrden( valor: string): void {
+		this.ordenarPor = valor;
+	}
 }
