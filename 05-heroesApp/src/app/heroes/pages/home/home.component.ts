6a5d66038dc0cb50ffd6ad616af81571a632f0e8
adrianblfe <adrianblfe@gmail.com>
2022-05-30 10:16:04 +0200

    Fin sección 15

--- 05-heroesApp/src/app/heroes/pages/home/home.component.ts
+++ 05-heroesApp/src/app/heroes/pages/home/home.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthService } from 'src/app/auth/services/auth.service';
 
 @Component({
 	selector: 'app-home',
@@ -13,9 +16,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-	constructor() { }
+	get auth() {
+		return this.authService.auth;
+	}
+
+	constructor(
+		private router: Router,
+		private authService: AuthService
+	) { }
 
 	ngOnInit(): void {
+	}
+
+	logout() {
+		// Ir al backend
+		// un usuario
+
+		this.router.navigate(['./auth']);
 	}
 
 }
