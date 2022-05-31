3c387b26434c5c5e07fe6fbe264ebd12e9f99807
adrianblfe <adrianblfe@gmail.com>
2022-05-31 16:26:50 +0200

    Fin sección 17

---
+++ 06-formulariosApp/src/app/reactive/dinamicos/dinamicos.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-dinamicos',
+  templateUrl: './dinamicos.component.html',
+  styles: [
+  ]
+})
+export class DinamicosComponent {
+
+	a!: number;
+
+	miFormulario: FormGroup = this.fb.group({
+		nombre: [ '', [ Validators.required, Validators.minLength(3) ] ],
+		favoritos: this.fb.array( [
+			[ 'Metal Gear', Validators.required ],
+			[ 'Final Fantasy', Validators.required ]
+		], Validators.required ),
+	});
+
+	nuevoFavorito: FormControl = this.fb.control( '', Validators.required );
+
+	get favoritosArr() {
+		return this.miFormulario.get('favoritos') as FormArray;
+	}
+
+	constructor( private fb: FormBuilder ) { }
+
+	campoValido( campo: string ) {
+		return this.miFormulario.controls[campo]?.errors &&
+			this.miFormulario.controls[campo]?.touched;
+	}
+
+	agregarFavorito() {
+		if ( this.nuevoFavorito.invalid ) { return; }
+
+		this.favoritosArr.push( this.fb.control( this.nuevoFavorito.value, Validators.required  ) );
+
+		this.nuevoFavorito.reset();
+	}
+
+	borrar( index: number ) {
+		this.favoritosArr.removeAt( index );
+	}
+
+	guardar() {
+		if ( this.miFormulario.invalid ) {
+			this.miFormulario.markAllAsTouched();
+			return;
+		}
+
+		console.log(this.miFormulario.value);
+	}
+
+}
