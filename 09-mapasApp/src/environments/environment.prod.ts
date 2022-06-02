189fe016c36fc43a9c690ae45adc66cdefe12ae3
adrianblfe <adrianblfe@gmail.com>
2022-06-02 10:23:58 +0200

    Fin sección 21

---
+++ 09-mapasApp/src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  mapBoxToken: 'pk.eyJ1IjoiYWJsYW5jbzg3IiwiYSI6ImNsM3ZvYjVjbjBidWkzZHFubHg4M2M5M3EifQ.z2M-5S0b20LYJC58d6Ba-g'
+};
---
+++ 09-mapasApp/src/environments/environment.ts
@@ -0,0 +1,17 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  mapBoxToken: 'pk.eyJ1IjoiYWJsYW5jbzg3IiwiYSI6ImNsM3ZvYjVjbjBidWkzZHFubHg4M2M5M3EifQ.z2M-5S0b20LYJC58d6Ba-g'
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
